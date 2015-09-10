79e58d89bd2455b222b0a34ced53eb872d90918d
Leo Baran <leobaran@cs.washington.edu>
2015-09-10 13:11:08 -0700

    text generation working, but words are repeated frequently.

--- markov.py
+++ markov.py
@@ -1,3 +1,4 @@
+import random
 
 #a markov chain-based text generator. 
 class Markov:
@@ -14,11 +15,30 @@ class Markov:
             else:
                 result[tuple].append(wordArray[i+2]);
 
-
         return result;
 
+    def printWords(self, number):
 
+        def getRandomWords():
+            return random.choice(list(self.chains.keys()));
+        
+        def getNextTuple(first, second):
+            possibleWords = self.chains[(first, second)];
+            if(len(possibleWords) == 0):
+                first, second = getRandomWords();
+                possibleWords = self.chains[(first, second)];
+
+            return (first, second, random.choice(possibleWords));
+
+        first, second = getRandomWords(); 
+        print(first + " ", end=''); 
+        for i in range(number):
+            first, second, newWord = getNextTuple(first, second);
+            print(second + " " + newWord + " ", end='');
+            first = second; 
+            second = newWord;
+            
 
 f = open('file.txt');
 m = Markov(f);
-print(m.chains);
+m.printWords(5);
