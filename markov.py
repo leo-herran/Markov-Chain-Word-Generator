1fdf84623e586dc0eef029ef38651a5278982f9b
Leo Baran <leobaran@cs.washington.edu>
2015-09-10 12:45:13 -0700

    basic file processing.

--- markov.py
+++ markov.py
@@ -2,13 +2,23 @@
 #a markov chain-based text generator. 
 class Markov:
 
-    def __init__(file):
-        self.data = file.readlines();
+    def __init__(self, file):
+        self.chains = self.processRawData(file.read().split());
+
+    def processRawData(self, wordArray):
+        result = {};
+        for i in range(len(wordArray)-2):
+            tuple = (wordArray[i], wordArray[i+1]);
+            if tuple not in result:
+                result[tuple] = [wordArray[i+2]];
+            else:
+                result[tuple].append(wordArray[i+2]);
+
+
+        return result;
 
 
 
-
-
-
-f = open('words.txt');
+f = open('file.txt');
 m = Markov(f);
+print(m.chains);
