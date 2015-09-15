273c44a92c29a38ad2265d00a24c3cfc4e711292
Leo Baran <leobaran@cs.washington.edu>
2015-09-15 12:35:21 -0700

    changed word generation structure. Works with python2 but word chains must be picked accordingly.

--- markov.py
+++ markov.py
@@ -26,8 +26,9 @@ class Markov:
 
         return result;
 
-    def printWords(self, number):
+    def getGeneratedWords(self, number):
 
+        
         def getRandomWords():
             return random.choice(list(self.chains.keys()));
         
@@ -40,7 +41,7 @@ class Markov:
             return (first, second, random.choice(possibleWords));
 
         first, second = getRandomWords(); 
-        print(first + " " + second + " ", end=''); 
+        result = first + " " + second + " "; 
         for i in range(number):
             #first, second, newWord = getNextTuple(first, second);
             if((first, second) not in self.chains):
@@ -48,28 +49,32 @@ class Markov:
             else:
                 newWord = random.choice(self.chains[(first, second)]);
 
-            print(newWord + " ", end='');
+            result = result + newWord + " "
             first, second = second, newWord; 
+
+        return result;
         
-        print();
-
-def main():
-    #first argument is the .py file, so exclude it. 
-    args = sys.argv[1:];
-
-    if(len(args) != 2):
-        print('Usage: python markov.py (dataFile) (number of words to output)');
-        return;
-
-    dataFile = args[0];
-    numWords = int(args[1]);
+def main(dataFile, numWords):
     if('.txt' in dataFile):
         m = Markov(open(dataFile, 'r'));
     else:
         m = Markov('', dataFile);
 
-    m.printWords(numWords);
+    words = m.getGeneratedWords(numWords);
+    print(words);
 
 if __name__ == "__main__":
-    main();
+
+    #first argument is the .py file, so exclude it. 
+    args = sys.argv[1:];
+
+    if(len(args) != 2):
+        print('Usage: python markov.py (dataFile) (number of words to output)');
+	
+    else:
+
+        dataFile = args[0];
+        numWords = int(args[1]);
+ 
+        main(dataFile, numWords);
 
