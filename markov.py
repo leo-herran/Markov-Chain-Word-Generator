c350e4bdd4680e665a10e3feb4003a22c6fe25e9
Leo Baran <leobaran@cs.washington.edu>
2015-09-15 11:24:04 -0700

    file data and number of words passed in as arguments

--- markov.py
+++ markov.py
@@ -1,13 +1,20 @@
-import random
+import random, pickle, sys
 
 #a markov chain-based text generator. 
 class Markov:
 
-    def __init__(self, file):
-        self.chains = self.processRawData(file.read().split());
-
+    def __init__(self, file, wordDataFile=None):
+        if(wordDataFile == None):
+            self.chains = self.processRawData(file.read().split());
+        else:
+            self.chains = pickle.load(open(wordDataFile, 'rb')); 
+    
     def processRawData(self, wordArray):
         result = {};
+
+        for i in range(len(wordArray)):
+            wordArray[i] = wordArray[i].lower();
+
         for i in range(len(wordArray)-2):
             tuple = (wordArray[i], wordArray[i+1]);
             newWord = wordArray[i+2];
@@ -46,6 +53,23 @@ class Markov:
         
         print();
 
-f = open(input());
-m = Markov(f);
-m.printWords(int(input()));
+def main():
+    #first argument is the .py file, so exclude it. 
+    args = sys.argv[1:];
+
+    if(len(args) != 2):
+        print('Usage: python markov.py (dataFile) (number of words to output)');
+        return;
+
+    dataFile = args[0];
+    numWords = int(args[1]);
+    if('.txt' in dataFile):
+        m = Markov(open(dataFile, 'r'));
+    else:
+        m = Markov('', dataFile);
+
+    m.printWords(numWords);
+
+if __name__ == "__main__":
+    main();
+
